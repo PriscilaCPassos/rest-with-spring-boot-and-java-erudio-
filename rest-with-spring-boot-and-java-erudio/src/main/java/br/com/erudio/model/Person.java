3686fae73721ca5c67d72961173d47cce7cb5fd4
PriscilaCPassos <116843833+PriscilaCPassos@users.noreply.github.com>
2024-01-31 10:04:11 -0300

    Atualização de códigos, organizando-os em seus respectivos packages.

---
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/model/Person.java
@@ -0,0 +1,86 @@
+package br.com.erudio.model;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+public class Person implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String gender;
+
+
+    public Person() {} // Constructor padrão que o JPA exige.
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+              return true;
+        if (!(o instanceof Person person))
+              return false;
+        if (!id.equals(person.id))
+              return false;
+        if (!firstName.equals(person.firstName))
+              return false;
+        if (!lastName.equals(person.lastName))
+              return false;
+        if (!address.equals(person.address))
+              return false;
+        return gender.equals(person.gender);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id.hashCode();
+        result = 31 * result + firstName.hashCode();
+        result = 31 * result + lastName.hashCode();
+        result = 31 * result + address.hashCode();
+        result = 31 * result + gender.hashCode();
+        return result;
+    }
+}
